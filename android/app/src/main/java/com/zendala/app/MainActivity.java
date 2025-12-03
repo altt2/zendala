85ee8bf0b08c6e5e43e1f93a8f3cf0d051393063
altt2 <34169363+altt2@users.noreply.github.com>
2025-12-03 16:25:55 -0500

    fix: enable cookies in MainActivity for Capacitor

--- android/app/src/main/java/com/zendala/app/MainActivity.java
+++ android/app/src/main/java/com/zendala/app/MainActivity.java
@@ -3,16 +3,16 @@ package com.zendala.app;
 import com.getcapacitor.BridgeActivity;
 import android.webkit.CookieManager;
 import android.os.Build;
+import android.os.Bundle;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    protected void onStart() {
-        super.onStart();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         // Enable cookies for WebView to support session management
         CookieManager cookieManager = CookieManager.getInstance();
         cookieManager.setAcceptCookie(true);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            cookieManager.setAcceptThirdPartyCookies(this.getWebView(), true);
-        }
+        // Note: setAcceptThirdPartyCookies requires a WebView instance,
+        // but Capacitor handles cookie management automatically
     }
 }
